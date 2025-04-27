c582ced5964dc1f94c2f5e479ef0c433d67f09d2
Yadunandana Acharya <51967645+Yadunandanaacharya@users.noreply.github.com>
2025-04-27 09:21:16 +0530

    changes

---
+++ StudentCrud/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCrud.Models
+{   //11:43
+    public class Student
+    {
+        [Key]
+        public int id { get;set; }
+        public string stName { get;set; }
+        public string Course { get;set; }
+
+    }
+}
